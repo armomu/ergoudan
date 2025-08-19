452624e8021d4185f1d630adaa4185d515a84a4c
陈华杰 <894620576@qq.com>
2025-08-19 13:58:39 +0800

    build: 7bf04404

---
+++ assets/tgaTextureLoader-0118a00d.js
@@ -0,0 +1,1 @@
+import{h as r,i as n}from"./babylonjs-99ece238.js";import"./index-e810d8cb.js";class d{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,a){const o=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),s=r(o);a(s.width,s.height,t.generateMipMaps,!1,()=>{n(t,o)})}}export{d as _TGATextureLoader};
